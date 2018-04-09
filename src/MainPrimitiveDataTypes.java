8b6fd23e0d8638e79a55b2c170f98266c6206760
Guilherme Ruschel Kelling <guilhermekelling@gmail.com>
2018-04-09 09:50:55 -0300

    Primeiro commit

---
+++ src/MainPrimitiveDataTypes.java
@@ -0,0 +1,16 @@
+
+public class MainPrimitiveDataTypes {
+
+	public static void main(String[] args) {
+		// byte
+		// short
+		// int
+		// long
+		// float
+		// double
+		// char
+		// boolean
+		
+	}
+	
+}
