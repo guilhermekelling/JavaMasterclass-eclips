6aac74176f00a0223609d1912aaf5ee93a020741
Guilherme Ruschel Kelling <guilhermekelling@gmail.com>
2018-04-23 09:58:03 -0300

    Section7: Classes Part1 and Part2

--- src/Section7_ClassesConstructorsInheritance/Main_Classes.java
+++ src/Section7_ClassesConstructorsInheritance/Main_Classes.java
@@ -7,6 +7,13 @@
 package Section7_ClassesConstructorsInheritance;
 
 public class Main_Classes {
+	
+	public static void main(String[] args) {
+		Car porsche = new Car();
+		Car holden = new Car();
+		porsche.setModel("Carrera");
+		System.out.println("Model is " + porsche.getModel());
+	}
 
 }
 
