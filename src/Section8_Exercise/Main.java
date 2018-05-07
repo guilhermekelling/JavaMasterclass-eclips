3e307a478c7df191950027b020fb0b3e5db98840
Guilherme Ruschel Kelling <guilhermekelling@gmail.com>
2018-05-07 14:46:18 -0300

    Section8: Exercise Finish

--- src/Section8_Exercise/Main.java
+++ src/Section8_Exercise/Main.java
@@ -38,6 +38,28 @@ public class Main {
         // For the two additional classes this may require you to be looking at the base class for pricing and then
         // adding totals to final price.
 		
+		Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+		double price = hamburger.itemizeHamburger();
+		hamburger.addHamburgerAddition1("Tomato", 0.27);
+		hamburger.addHamburgerAddition2("Lettuce",  0.75);
+		hamburger.addHamburgerAddition3("Cheese", 1.12);
+		System.out.println("Total Burger price is " + hamburger.itemizeHamburger());
+		
+		HealthyBurger healthyBurger = new HealthyBurger("Bacon", 5.67);
+		healthyBurger.addHamburgerAddition1("Egg", 5.43);
+		healthyBurger.addHealthAddition1("Lentils", 3.41);
+		System.out.println("Total Healthy Burger price is " + healthyBurger.itemizeHamburger());
+		
+		DeluxeBurger db = new DeluxeBurger();
+		db.addHamburgerAddition3("Should not do this", 50.53);
+		db.itemizeHamburger();
+		
+		
+		
+		 
+		
+		
+		
 	}
 
 }
