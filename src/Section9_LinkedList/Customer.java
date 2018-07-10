3329bb265d224d2d157ecc32b00e5e7e882d9c43
Guilherme Ruschel Kelling <guilhermekelling@gmail.com>
2018-07-10 11:02:59 -0300

    Section9: LinkedList - Part 1,2,3 - finish

---
+++ src/Section9_LinkedList/Customer.java
@@ -0,0 +1,36 @@
+/*
+ * @author: Guilherme Ruschel Kelling 
+ *  
+ * 
+ */
+
+package Section9_LinkedList;
+
+public class Customer {
+	private String name;
+	private double balance;
+	
+	public Customer(String name, double balance) {
+		this.name = name;
+		this.balance = balance;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+
+}
+
